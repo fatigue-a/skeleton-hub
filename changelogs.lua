7dfbb33078e9418bc4677136aa798e08d4c4cce5
fatigue-a <devinv2409@gmail.com>
2025-05-14 02:32:08 -0400

    Update changelogs.lua

--- changelogs.lua
+++ changelogs.lua
@@ -1,11 +1,26 @@
 return {
     ["Grow a Garden"] = {
-        "Initial release with AutoFarm and AutoSell",
-        "Added Moonlit submission",
-        "Improved proximity prompt distance",
+        ["v1.0"] = {
+            "First version with AutoFarm and AutoSell",
+        },
+        ["v1.1"] = {
+            "Improved Auto Collect",
+            "Added Auto Moonlit submission",
+            "Fixed bug with AutoSell",
+        },
+        ["v1.2"] = {
+            "Added Farm Stats",
+            "Better Changelogs",
+            "Added Walkspeed, and Jumpower to the misc tab",
+        },
     },
-    ["Test"] = {
-        "Testing",
-        "Testing2",
+    ["PlaceHolder"] = {
+        ["v1.0"] = {
+            "Testing",
+            "Testing",
+        },
+        ["v1.1"] = {
+            "test",
+        },
     }
 }
