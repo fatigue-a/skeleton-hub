02b1eaaffbdf58d9d24285886c638d072ce74bda
fatigue-a <devinv2409@gmail.com>
2025-05-14 02:24:03 -0400

    Update and rename changelogs to changelogs.lua

---
+++ changelogs.lua
@@ -0,0 +1,11 @@
+local allChangelogs = {
+    ["Grow a Garden"] = {
+        ["v1.0"] = "First release with basic AutoFarm.",
+        ["v1.1"] = "Added AutoSell and Moonlit Submit.",
+        ["v1.2"] = "Improved teleporting and bug fixes."
+    },
+    ["Test"] = {
+        ["v1.0"] = "Test",
+        ["v1.1"] = "Test2"
+    }
+}
